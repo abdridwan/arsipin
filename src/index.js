909d53dacd385d85f24c39e9d1f662d9c61c62be
BPS Sampang <abdullahridwan96@gmail.com>
2026-09-16 08:55:27 +0700

    fix(deps): update to official whatsapp-web.js@1.34.7 to fix window.require error natively

--- src/index.js
+++ src/index.js
@@ -12,16 +12,15 @@ const client = new Client({
     clientId: "arsipin-bot",
     dataPath: ".wwebjs_auth",
   }),
-  webVersionCache: {
-    type: "remote",
-    remotePath: "https://raw.githubusercontent.com/wppconnect-team/wa-version/7f959f352158729c6e88dd77cf7d38447d7be713/html/2.2412.54.html",
-  },
   puppeteer: {
     headless: true,
     args: [
       "--no-sandbox",
       "--disable-setuid-sandbox",
       "--disable-dev-shm-usage",
+      "--disable-gpu",
+      "--disable-software-rasterizer",
+      "--disable-webgl",
     ],
   },
 })
